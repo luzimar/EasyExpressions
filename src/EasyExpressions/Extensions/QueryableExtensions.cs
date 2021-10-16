60f899716eaada1b66a2579912fc3ce80273a629
Luzimar Pereira de Oliveira <luzimar.oliveira@ndd.com.br>
2021-10-16 10:35:44 -0400

    add operator Or and tests

--- src/EasyExpressions/Extensions/QueryableExtensions.cs
+++ src/EasyExpressions/Extensions/QueryableExtensions.cs
@@ -11,5 +11,11 @@ namespace EasyExpressions.Extensions
             InvocationExpression invocationExpression = Expression.Invoke(expression2, expression1.Parameters.Cast<Expression>());
             return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expression1.Body, invocationExpression), expression1.Parameters);
         }
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expression1, Expression<Func<T, bool>> expression2)
+        {
+            InvocationExpression invocationExpression = Expression.Invoke(expression2, expression1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>(Expression.Or(expression1.Body, invocationExpression), expression1.Parameters);
+        }
     }
 }
--- tests/EasyExpressions.Test/Extensions/QueryableExtensionsTests.cs
+++ tests/EasyExpressions.Test/Extensions/QueryableExtensionsTests.cs
@@ -138,5 +138,125 @@ namespace EasyExpressions.Test.Extensions
 
             #endregion
         }
+
+        [Fact]
+        public void FilterByFirstOrLastNameValidateNotEmpty()
+        {
+            #region Arrange
+
+            string firstName = "James";
+            string lastName = "Doe";
+
+            filter = x => x.FirstName.Equals(firstName);
+
+            #endregion
+
+            #region Act
+
+            if (!string.IsNullOrEmpty(lastName))
+                filter = filter.Or(x => x.LastName.Equals(lastName));
+
+            var result = PeopleMock.GetPeoples().AsQueryable().Where(filter).ToList();
+
+            #endregion
+
+            #region Assert
+
+            result.Count().Should().Be(1, "Failed test of filter by first and last name");
+
+            #endregion
+        }
+
+        [Fact]
+        public void FilterByFirstOrLastNameOrAgeValidateNotEmpty()
+        {
+            #region Arrange
+
+            string firstName = "James";
+            string lastName = "Monroe";
+            int? age = 30;
+
+            filter = x => x.FirstName.Equals(firstName);
+
+            #endregion
+
+            #region Act
+
+            if (!string.IsNullOrEmpty(lastName))
+                filter = filter.Or(x => x.LastName.Equals(lastName));
+
+            if (age.HasValue)
+                filter = filter.Or(x => x.Age == age.Value);
+
+            var result = PeopleMock.GetPeoples().AsQueryable().Where(filter).ToList();
+
+            #endregion
+
+            #region Assert
+
+            result.Count().Should().Be(1, "Failed test of filter by first and last name and age");
+
+            #endregion
+        }
+
+        [Fact]
+        public void FilterByFirstOrLastNameValidateEmpty()
+        {
+            #region Arrange
+
+            string firstName = "James";
+            string lastName = "Monroe";
+
+            filter = x => x.FirstName.Equals(firstName);
+
+            #endregion
+
+            #region Act
+
+            if (!string.IsNullOrEmpty(lastName))
+                filter = filter.Or(x => x.LastName.Equals(lastName));
+
+            var result = PeopleMock.GetPeoples().AsQueryable().Where(filter).ToList();
+
+            #endregion
+
+            #region Assert
+
+            result.Count().Should().Be(0, "Failed test of filter by first and last name");
+
+            #endregion
+        }
+
+        [Fact]
+        public void FilterByFirstOrLastNameOrAgeValidateEmpty()
+        {
+            #region Arrange
+
+            string firstName = "James";
+            string lastName = "Monroe";
+            int? age = 28;
+
+            filter = x => x.FirstName.Equals(firstName);
+
+            #endregion
+
+            #region Act
+
+            if (!string.IsNullOrEmpty(lastName))
+                filter = filter.Or(x => x.LastName.Equals(lastName));
+
+            if (age.HasValue)
+                filter = filter.Or(x => x.Age == age.Value);
+
+            var result = PeopleMock.GetPeoples().AsQueryable().Where(filter).ToList();
+
+            #endregion
+
+            #region Assert
+
+            result.Count().Should().Be(0, "Failed test of filter by first and last name and age");
+
+            #endregion
+        }
     }
 }
